0c440556fd668f54060fce729c62be808a2989fa
kubisma <213828029+kubisma@users.noreply.github.com>
2025-06-08 15:15:28 +0200

    Add files via upload

---
+++ src/theme/colors.js
@@ -0,0 +1,11 @@
+export default {
+  primary: "#6851A4",
+  onPrimary: "#ffffff", 
+  background: "#f5f5f5", 
+  surface: "#ffffff",
+  error: "#D94F4F", 
+  text: "#000000", 
+  placeholder: "#9e9e9e", 
+  border: "#e0e0e0",
+  accent: "7f473b", 
+};
