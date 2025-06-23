b0fc5a25cb89b80223329bfd033fceddc220e043
kubisma <213828029+kubisma@users.noreply.github.com>
2025-06-23 14:56:53 +0200

    Update eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -1,12 +1,21 @@
-// https://docs.expo.dev/guides/using-eslint/
 const { defineConfig } = require("eslint/config");
-const expoConfig = require("eslint-config-expo/flat");
-const eslintPluginPrettierRecommended = require('eslint-plugin-prettier/recommended');
+const expo = require("eslint-config-expo/flat");
+const prettierPlugin = require("eslint-plugin-prettier");
 
 module.exports = defineConfig([
-  expoConfig,
-  eslintPluginPrettierRecommended,
+  expo,
+
   {
-    ignores: ['dist/*'],
+    name: "prettier",
+    plugins: {
+      prettier: prettierPlugin,
+    },
+    rules: {
+      "prettier/prettier": "error",
+    },
+  },
+
+  {
+    ignores: ["**/dist/**", "**/node_modules/**"],
   },
 ]);
