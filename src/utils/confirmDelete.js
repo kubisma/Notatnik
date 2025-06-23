efebda091f76d23e161495c39573982b85be81b8
kubisma <213828029+kubisma@users.noreply.github.com>
2025-06-23 15:00:14 +0200

    Update confirmDelete.js
    Poprawki formatowania kodu

--- src/utils/confirmDelete.js
+++ src/utils/confirmDelete.js
@@ -1,14 +1,14 @@
-import { Alert } from 'react-native';
+import { Alert } from "react-native";
 
 //Wyświetla alert potwierdzający usunięcie notatki
 export const confirmDelete = (onConfirm) => {
   Alert.alert(
-    'Usuń notatkę',
-    'Czy na pewno chcesz usunąć notatkę?',
+    "Usuń notatkę",
+    "Czy na pewno chcesz usunąć notatkę?",
     [
-      { text: 'Anuluj', style: 'cancel' },
-      { text: 'Usuń', style: 'destructive', onPress: onConfirm },
+      { text: "Anuluj", style: "cancel" },
+      { text: "Usuń", style: "destructive", onPress: onConfirm },
     ],
-    { cancelable: true }
+    { cancelable: true },
   );
 };
