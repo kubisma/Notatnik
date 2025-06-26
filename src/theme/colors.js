8ebef05352db06e4d4dd40108f1ae009f4f721e6
kubisma <213828029+kubisma@users.noreply.github.com>
2025-06-26 13:41:36 +0200

    Update colors.js
    Dodano komentarze

--- src/theme/colors.js
+++ src/theme/colors.js
@@ -1,11 +1,11 @@
 export default {
-  primary: "#6851A4",
-  onPrimary: "#ffffff",
-  background: "#f5f5f5",
-  surface: "#ffffff",
-  error: "#D94F4F",
-  text: "#000000",
-  placeholder: "#9e9e9e",
-  border: "#e0e0e0",
-  accent: "7f473b",
+  primary: "#6851A4", // Główny kolor aplikacji (np. przyciski, nagłówki)
+  onPrimary: "#ffffff", // Kolor tekstu/ikon na tle primary (kontrastujący)
+  background: "#f5f5f5", // Kolor tła całej aplikacji
+  surface: "#ffffff", // Kolor tła elementów UI (np. karty, modale)
+  error: "#D94F4F", // Kolor do komunikatów o błędach
+  text: "#000000", // Główny kolor tekstu
+  placeholder: "#9e9e9e", // Kolor podpowiedzi w polach tekstowych
+  border: "#e0e0e0", // Kolor ramek, separatorów
+  accent: "#7f473b", // Dodatkowy kolor akcentowy (opcjonalny)
 };
